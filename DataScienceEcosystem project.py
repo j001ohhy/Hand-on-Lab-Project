5f301140ba48ae1b9f143a62107cfcb10d099972
j001ohhy <162704811+j001ohhy@users.noreply.github.com>
2024-03-14 10:17:54 +0100

    Add files via upload

---
+++ DataScienceEcosystem project.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Exercise 2: 
+# # Data Science Tools and Ecosystem
+# 
+
+# Exercise 3:
+# In this notebook, Data Science Tools and Ecosystem are summarize
+
+# Exercise 4:
+# **Some of the popular languages that Data Scientists use are:**
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 5. Scala
+
+# Exercise 5: 
+# ***Some of the commonly used libraries by Data Scientist include:***
+# 1. Numpy
+# 2. Pandas
+# 3. Matplotlib
+# 4. Seaborn
+# 5. TensorFlow
+# 6. Scipy
+# 7. Keras
+
+# Exercise 6: 
+# **DATA  SCIENCE TOOLS**
+# 
+# |Data Science Tools|
+# |-----|
+# |Jupyter Notebook|
+# |TensorFlow|
+# |Apache Spark|
+# 
+
+#  Exercise 7:
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+#Exercise 8:
+#This is a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[9]:
+
+
+#Exercise 9:
+x = int (input ("Enter your value in 200: "))
+print ("My value is: ", x)
+y = 60 #minutes
+print ("I want to divide", x, "by 60")
+z = x/y
+print ("The result = ", z, "hours")
+
+
+# Exercise 10:
+# ### In this notebook, Data Science Tools and Ecosystem are summarize
+# **LIst popular languages for Data Science:**
+# - Python 
+# - R
+# - SQL
+# - Julia
+# - Scala
+
+# Exercise 11:
+# ## Author
+# ##### John
+
+# In[ ]:
+
+
+
+
